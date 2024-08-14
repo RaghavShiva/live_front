05984088aa1a8c3291cafdf17967aa6eee89bd2e
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2024-08-14 17:49:51 +0530

    Update Chat.js

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -10,7 +10,7 @@ import Input from '../Input/Input';
 
 import './Chat.css';
 
-const ENDPOINT = 'https://livelingo.onrender.com/';
+const ENDPOINT = 'https://livelingo.onrender.com';
 
 let socket;
 
